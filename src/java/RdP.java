38383e529af6360243e9031c15ab10db5fac1910
alexfyh <alexander.fyh@gmail.com>
2017-08-07 02:01:28 -0300

    RdP sin verificaion de sensiilidad

--- src/java/RdP.java
+++ src/java/RdP.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class RdP{
   private Matriz marcadoInicial;
   private Matriz marcadoActual;
@@ -7,12 +9,13 @@ public class RdP{
 
   public RdP(Matriz marcadoInicial,Matriz incidenciaPrevia,Matriz incidenciaPosterior){
     try{
+      //if (mInicial==null) throw
       this.marcadoInicial = marcadoInicial;
-      this.marcadoActual = marcadoActual;
+      this.marcadoActual = marcadoInicial;
       this.incidenciaPrevia = incidenciaPrevia;
       this.incidenciaPosterior = incidenciaPosterior;
       this.incidencia = Matriz.suma(this.incidenciaPosterior,Matriz.porEscalar(this.incidenciaPrevia,-1));
-      
+
 
     }
     catch(Exception e){
@@ -22,6 +25,28 @@ public class RdP{
   }
   public Matriz getIncidencia(){
     return this.incidencia;
+  }
+  public List<Integer> sensibilizadas(){
+    return null;
+  }
+  public Matriz marcadoInicial(){
+    return this.marcadoInicial;
+  }
+  public Matriz marcadoActual(){
+    return this.marcadoActual;
+  }
+  public void disparar(int x) throws RdPException{
+    try{
+      if(x<0||x>this.incidencia.getMatriz()[0].length){
+        throw new RdPException("Transicion no valida.");
+      }
+      this.marcadoActual = Matriz.suma(this.marcadoActual,Matriz.obtenerColumna(this.incidencia,x));
+    }
+    catch(Exception e)
+    {
+      throw new RdPException(e.getMessage());
+    }
+
   }
 
 
--- src/java/RdPTest.java
+++ src/java/RdPTest.java
@@ -13,12 +13,29 @@ public class RdPTest{
                         {0,0,0},
                         {0,0,1}};
 
-      RdP petri = new RdP(null, new Matriz(incPre), new Matriz(incPos));
-      petri.getIncidencia().imprimir();
+      int[][] inicial = {{1},
+                          {1},
+                          {0},
+                          {0}};
+      //Matriz inicial= new Matriz(mInicial);
+      //inicial.imprimir();
+      RdP petri = new RdP(new Matriz(inicial), new Matriz(incPre), new Matriz(incPos));
+      System.out.println("Marcados: ");
+      petri.marcadoInicial().imprimir();
+      petri.disparar(0);
+      petri.marcadoActual().imprimir();
+      petri.disparar(1);
+      petri.marcadoActual().imprimir();
+      petri.disparar(2);
+      petri.marcadoActual().imprimir();
+      petri.disparar(2);
+      petri.marcadoActual().imprimir();
+      //petri.getIncidencia().imprimir();
 
 
     }
     catch(Exception e){
+      System.out.println(e.getMessage());
 
     }
 
