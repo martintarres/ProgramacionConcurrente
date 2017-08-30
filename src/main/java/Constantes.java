fa53245863be4246016d3ab84374d5e2b145918a
alexfyh <alexander.fyh@gmail.com>
2017-08-30 04:06:07 -0300

    Constante lee el html desde el directorio  raiz

--- src/main/java/Constantes.java
+++ src/main/java/Constantes.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
+import java.io.File;
 
 public class Constantes {
 
@@ -15,205 +16,221 @@ public class Constantes {
 
   // por que no declare cuanto valia las plazas y las transiciones y armaba de una la mtraiz
   public Constantes() {
+    try{
+      String file= "file:///";
+      String path = (new File(".")).getCanonicalPath();
+      System.out.println("Ruta actual ");
+      System.out.println(file+path);
+      String archivo = "/archivo.html";
+      String invariantes ="/analisisInvariante.html";
+      System.out.println(file+path+archivo);
+      int inci1 = 0;  // Para calcular numero de filas de las matrices
+      int inci2 = -1; // Para calcular numero de columnas de lsa matrices
 
-    int inci1 = 0;  // Para calcular numero de filas de las matrices
-    int inci2 = -1; // Para calcular numero de columnas de lsa matrices
-
-    int TotalPInva= -1;  // Para calcular el numero de filas de la matriz de P invariantes
-
-    Lector lector = new Lector("file:///home/martin/Documentos/ProgramacionConcurrente/archivoPrueba.html");
-    Lector lector1 = new Lector("file:///home/martin/Documentos/ProgramacionConcurrente/InvariantePruebas.html");
-    lector.convertir();
-    lector1.convertir();
-
-
-    String[][] Posterior = lector.getTabla(lector.cortar("Forwards incidence matrix I+", "Backwards incidence matrix I-"));
-    for (int i = 0; i < 1; i++) {
-      for (int j = 0; j < lector.cantidadColumnas(lector.cortar("Forwards incidence matrix I+", "Backwards incidence matrix I-")); j++) {
-        if (Posterior[i][j].contains("T")) {
-          // System.out.println(Posterior[i][j]);
-          arregloT.add(inci1);
-          //System.out.println("Voy a mostrar mi vector arregloT " + arregloT.elementAt(inci1));
-          inci1++;
+      int TotalPInva= -1;  // Para calcular el numero de filas de la matriz de P invariantes
 
 
-        }
-
-      }
-      //System.out.println("\n");
-
-    }
-
-    // String[][] tablaPosterior = lector.getTabla(lector.cortar("Forwards incidence matrix I+","Backwards incidence matrix I-"));
-    for (int i = 0; i < lector.cantidadFilas(lector.cortar("Forwards incidence matrix I+", "Backwards incidence matrix I-")); i++) {
-      for (int j = 0; j < 1; j++) {
-        // System.out.println(tablaPevia[i][j]);
-        if (inci2 != -1) {
-          arregloMP.add(inci2);
-          //System.out.println("Voy a mostrar mi vector arregloMP " + arregloMP.elementAt(inci2));
-        }
-
-        inci2++;
-      }
-      // System.out.println("\n");
-
-    }
 
 
-    System.out.println("");
-    System.out.println("");
-    System.out.println("");
-
-    int[][] posterior = new int[inci2][inci1];
-
-    System.out.println("voy a mostrar matriz de incidendia posterior");
+      Lector lector = new Lector(file+path+archivo);
+      Lector lector1 = new Lector(file+path+invariantes);
+      lector.convertir();
+      lector1.convertir();
 
 
-    for (int i = 0; i < inci2 + 1; i++) {
-      for (int j = 0; j < inci1 + 1; j++) {
+      String[][] Posterior = lector.getTabla(lector.cortar("Forwards incidence matrix I+", "Backwards incidence matrix I-"));
+      for (int i = 0; i < 1; i++) {
+        for (int j = 0; j < lector.cantidadColumnas(lector.cortar("Forwards incidence matrix I+", "Backwards incidence matrix I-")); j++) {
+          if (Posterior[i][j].contains("T")) {
+            // System.out.println(Posterior[i][j]);
+            arregloT.add(inci1);
+            //System.out.println("Voy a mostrar mi vector arregloT " + arregloT.elementAt(inci1));
+            inci1++;
 
-        if (esNumero(Posterior[i][j])) {
 
-          //  System.out.print(tablaPosterior[i][j]);
-          posterior[i - 1][j - 1] = Integer.parseInt(Posterior[i][j]);
-          // System.out.print(Integer.parseInt(Posterior[i][j]));
+          }
 
         }
+        //System.out.println("\n");
 
       }
-      //System.out.println("\n");
-    }
 
-    try {
-      this.incidenciaPosterior = new Matriz(posterior);
-    } catch (MatrizException e) {
-      e.printStackTrace();
-    }
+      // String[][] tablaPosterior = lector.getTabla(lector.cortar("Forwards incidence matrix I+","Backwards incidence matrix I-"));
+      for (int i = 0; i < lector.cantidadFilas(lector.cortar("Forwards incidence matrix I+", "Backwards incidence matrix I-")); i++) {
+        for (int j = 0; j < 1; j++) {
+          // System.out.println(tablaPevia[i][j]);
+          if (inci2 != -1) {
+            arregloMP.add(inci2);
+            //System.out.println("Voy a mostrar mi vector arregloMP " + arregloMP.elementAt(inci2));
+          }
 
-
-    incidenciaPosterior.imprimir();
-
-
-    System.out.println("");
-    System.out.println("");
-    System.out.println("");
-
-    int[][] previa = new int[inci2][inci1];
-    System.out.println("voy a mostrar tabla previa ");
-    String[][] tablaPrevia = lector.getTabla(lector.cortar("Backwards incidence matrix I-", "Combined incidence matrix I"));
-    for (int i = 0; i < inci2 + 1; i++) {
-      for (int j = 0; j < inci1 + 1; j++) {
-        if (esNumero(tablaPrevia[i][j])) {
-
-          //System.out.print(tablaPrevia[i][j]);
-          previa[i - 1][j - 1] = Integer.parseInt(tablaPrevia[i][j]);
+          inci2++;
         }
-      }
-      // System.out.println("\n");
-
-    }
-
-    try {
-      this.incidenciaPrevia = new Matriz(previa);
-    } catch (MatrizException e) {
-      e.printStackTrace();
-    }
-
-    incidenciaPrevia.imprimir();
-
-
-    System.out.println("");
-    System.out.println("");
-    System.out.println("");
-
-    int[][] incial = new int[inci2][1];
-    System.out.println("voy a mostrar marcado incial ");
-
-    String[][] tablaInicial = lector.getTabla(lector.cortar("Marking", "Current"));
-    for (int i = 0; i < lector.cantidadFilas(lector.cortar("Marking", "Current")); i++) {
-      for (int j = 0; j < inci2 + 1; j++) {
-
-        if (esNumero(tablaInicial[i][j])) {
-          incial[j - 1][i - 1] = Integer.parseInt(tablaInicial[i][j]);
-          //  System.out.print(tablaInicial[i][j]);
-        }
+        // System.out.println("\n");
 
       }
+
+
       System.out.println("");
-    }
+      System.out.println("");
+      System.out.println("");
 
-    try {
-      this.marcadoInicial = new Matriz(incial);
-    } catch (MatrizException e) {
-      e.printStackTrace();
-    }
+      int[][] posterior = new int[inci2][inci1];
 
-    marcadoInicial.imprimir();
+      System.out.println("voy a mostrar matriz de incidendia posterior");
 
 
-    System.out.println("");
-    System.out.println("");
-    System.out.println("");
+      for (int i = 0; i < inci2 + 1; i++) {
+        for (int j = 0; j < inci1 + 1; j++) {
 
+          if (esNumero(Posterior[i][j])) {
 
+            //  System.out.print(tablaPosterior[i][j]);
+            posterior[i - 1][j - 1] = Integer.parseInt(Posterior[i][j]);
+            // System.out.print(Integer.parseInt(Posterior[i][j]));
 
-    System.out.println("voy a mostrar P invariantes ");
-
-    List <Integer> lista = new ArrayList <Integer>();
-
-
-  String[][] PInvariantes= lector1.getTabla(lector1.cortar("P-Invariants","P-Invariant equations"));
-
-    for (int i = 0; i < lector1.cantidadFilas(lector1.cortar("P-Invariants", "P-Invariant equations")); i++) {
-      for(int j=0; j< 1;j++){
-
-        TotalPInva++;
-      }
-
-
-    }
-
-
-
-    int[][] copia = new int[TotalPInva][inci2];
-
-
-    int contadorr = 0;
-    for (int i = 0; i < TotalPInva+1; i++) {      // aca hago TotalPInva+1 porque aun no saque las letras.
-    //  System.out.println("soy i " + i);         //  entonces tengo una fila mas
-      for (int j = 0; j < inci2+1 ; j++) {
-    //   System.out.println("soy j " + j);
-     //   System.out.println();
-        if (esNumero(PInvariantes[i][j])) {
-         // System.out.println("soy i de if" + i);
-          lista.add(Integer.parseInt(PInvariantes[i][j]));
-          // hola.add(Integer.parseInt(tumama[i][j]));
-          //System.out.print(PInvariantes[i][j]);
+          }
 
         }
+        //System.out.println("\n");
       }
-    }
 
-    for (int i = 0; i < TotalPInva; i++) {
+      try {
+        this.incidenciaPosterior = new Matriz(posterior);
+      } catch (MatrizException e) {
+        e.printStackTrace();
+      }
 
-      for (int j = 0; j < inci2; j++) {
 
-        copia[i][j] = lista.get(0);
+      incidenciaPosterior.imprimir();
+
+
+      System.out.println("");
+      System.out.println("");
+      System.out.println("");
+
+      int[][] previa = new int[inci2][inci1];
+      System.out.println("voy a mostrar tabla previa ");
+      String[][] tablaPrevia = lector.getTabla(lector.cortar("Backwards incidence matrix I-", "Combined incidence matrix I"));
+      for (int i = 0; i < inci2 + 1; i++) {
+        for (int j = 0; j < inci1 + 1; j++) {
+          if (esNumero(tablaPrevia[i][j])) {
+
+            //System.out.print(tablaPrevia[i][j]);
+            previa[i - 1][j - 1] = Integer.parseInt(tablaPrevia[i][j]);
+          }
+        }
+        // System.out.println("\n");
+
+      }
+
+      try {
+        this.incidenciaPrevia = new Matriz(previa);
+      } catch (MatrizException e) {
+        e.printStackTrace();
+      }
+
+      incidenciaPrevia.imprimir();
+
+
+      System.out.println("");
+      System.out.println("");
+      System.out.println("");
+
+      int[][] incial = new int[inci2][1];
+      System.out.println("voy a mostrar marcado incial ");
+
+      String[][] tablaInicial = lector.getTabla(lector.cortar("Marking", "Current"));
+      for (int i = 0; i < lector.cantidadFilas(lector.cortar("Marking", "Current")); i++) {
+        for (int j = 0; j < inci2 + 1; j++) {
+
+          if (esNumero(tablaInicial[i][j])) {
+            incial[j - 1][i - 1] = Integer.parseInt(tablaInicial[i][j]);
+            //  System.out.print(tablaInicial[i][j]);
+          }
+
+        }
+        System.out.println("");
+      }
+
+      try {
+        this.marcadoInicial = new Matriz(incial);
+      } catch (MatrizException e) {
+        e.printStackTrace();
+      }
+
+      marcadoInicial.imprimir();
+
+
+      System.out.println("");
+      System.out.println("");
+      System.out.println("");
+
+
+
+      System.out.println("voy a mostrar P invariantes ");
+
+      List <Integer> lista = new ArrayList <Integer>();
+
+
+      String[][] PInvariantes= lector1.getTabla(lector1.cortar("P-Invariants","P-Invariant equations"));
+
+      for (int i = 0; i < lector1.cantidadFilas(lector1.cortar("P-Invariants", "P-Invariant equations")); i++) {
+        for(int j=0; j< 1;j++){
+
+          TotalPInva++;
+        }
+
+
+      }
+
+
+
+      int[][] copia = new int[TotalPInva][inci2];
+
+
+      int contadorr = 0;
+      for (int i = 0; i < TotalPInva+1; i++) {      // aca hago TotalPInva+1 porque aun no saque las letras.
+        //  System.out.println("soy i " + i);         //  entonces tengo una fila mas
+        for (int j = 0; j < inci2+1 ; j++) {
+          //   System.out.println("soy j " + j);
+          //   System.out.println();
+          if (esNumero(PInvariantes[i][j])) {
+            // System.out.println("soy i de if" + i);
+            lista.add(Integer.parseInt(PInvariantes[i][j]));
+            // hola.add(Integer.parseInt(tumama[i][j]));
+            //System.out.print(PInvariantes[i][j]);
+
+          }
+        }
+      }
+
+      for (int i = 0; i < TotalPInva; i++) {
+
+        for (int j = 0; j < inci2; j++) {
+
+          copia[i][j] = lista.get(0);
 
 
           lista.remove(0);
 
 
+        }
       }
+
+      try {
+        this.PInvariante= new Matriz(copia);
+      } catch (MatrizException e) {
+        e.printStackTrace();
+      }
+
+      PInvariante.imprimir();
+
+    }
+    catch(Exception e){
+
     }
 
-    try {
-      this.PInvariante= new Matriz(copia);
-    } catch (MatrizException e) {
-      e.printStackTrace();
-    }
-
-    PInvariante.imprimir();
 
 
 
