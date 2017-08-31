f5a9d9d81e45252d34676539c2dd0a6a2e4646e0
alexfyh <alexander.fyh@gmail.com>
2017-08-31 03:10:51 -0300

    Toda la inicialicacion de las listas de enteros de los hilos está corrida aListas de Disparos

---
+++ src/main/java/ListasDeDisparos.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by YepezHinostroza on 31/8/2017.
+ */
+public  class ListasDeDisparos {
+    public List<Integer> l1= new ArrayList<Integer>();
+    public List<Integer> l2= new ArrayList<Integer>();
+    public List<Integer> l3= new ArrayList<Integer>();
+    public List<Integer> l4= new ArrayList<Integer>();
+    public List<Integer> l5= new ArrayList<Integer>();
+    public List<Integer> l6= new ArrayList<Integer>();
+    public List<Integer> l7= new ArrayList<Integer>();
+    public List<Integer> l8= new ArrayList<Integer>();
+    public List<Integer> l9= new ArrayList<Integer>();
+
+
+    public ListasDeDisparos(){
+        l1= new ArrayList<Integer>();
+        l2= new ArrayList<Integer>();
+        l3= new ArrayList<Integer>();
+        l4= new ArrayList<Integer>();
+        l5= new ArrayList<Integer>();
+        l6= new ArrayList<Integer>();
+        l7= new ArrayList<Integer>();
+        l8= new ArrayList<Integer>();
+        l9= new ArrayList<Integer>();
+
+        l1.add(10);
+        l1.add(11);
+
+
+        l2.add(12);
+        l2.add(13);
+
+        l3.add(0);
+
+
+        l4.add(1);
+
+
+        l5.add(3);
+        l5.add(5);
+        l5.add(7);
+
+        l6.add(2);
+        l6.add(4);
+        l6.add(6);
+        l6.add(8);
+
+        l7.add(9);
+
+        l8.add(18);
+        l8.add(19);
+
+        l9.add(14);
+        l9.add(15);
+        l9.add(16);
+        l9.add(17);
+
+
+
+    }
+
+}
