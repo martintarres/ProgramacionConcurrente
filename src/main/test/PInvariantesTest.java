7f1f8ce0d2bc58bfdcd5ca16336c6bf251fa3ae4
Martin Tarres <martintarres@gmail.com>
2017-10-10 11:55:14 -0300

    cambie el nombre a nosequeTest

--- src/main/test/PInvariantesTest.java
+++ src/main/test/PInvariantesTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 
-public class noSeQueTest {
+public class PInvariantesTest {
     Constantes constantes;
     Monitor monitor;
     List fin;
